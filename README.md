85abe9479c0c48c96b62a811286531ab85476c4a
Rodrigo Waltenberg <rwaltenberg@gmail.com>
2016-09-12 21:37:29 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,52 @@
+# Angular Money Mask
+An AngularJS (1.x) directive to mask inputs with currency values
+
+## Usage
+
+#### Reference script file
+
+```html
+<script src="rw-money-mask.min.js"></script>
+```
+
+#### Inject `rw.moneymask` module
+
+```javascript
+angular.module('yourApp', ['rw.moneymask'])
+```
+
+#### Bind directive `money-mask`
+
+```html
+<input type="text" name="price" ng-model="model.price" money-mask />
+```
+*Obs: __ngModel__ is required!*
+
+## How it works
+
+#### Model
+
+ - Expects it to be a valid Javascript `Number`. e.g.: `100` ($ 100.00), `4.20` ($ 4.20), `4340.65` ($ 4,340.65)
+ - It maintains the model value as a number. Only the input view gets formatted.
+
+#### View
+
+ - Displays formatted model value as the user types
+ - It will allow only numbers to the input. Each time a number is put, the value updates.
+ - Uses `$filter('number')`, which uses `$locale.NUMBER_FORMATS` configuration (separators, fraction size, etc) so it's i18n compatible.
+
+#### Options
+
+Use `money-mask-append` and `money-mask-prepend` to add some text to the formatted currency.
+```html
+<input type="text" name="price" ng-model="model.price" money-mask money-mask-prepend="$" />
+```
+or
+```html
+<input type="text" name="price" ng-model="model.price" money-mask money-mask-append="dollars" />
+```
+
+## Development - Contribute!
+
+- Improvements are always welcome. Fork it and send a pull request!
+- English revisions to this document are more than welcome :)
---
+++ rw-money-mask.js
@@ -0,0 +1,68 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('rw.moneymask', [])
+    .directive('moneyMask', moneyMask);
+
+  moneyMask.$inject = ['$filter'];
+  function moneyMask($filter) {
+    var directive = {
+      require: 'ngModel',
+      link: link,
+      restrict: 'A',
+      scope: {
+        model: '=ngModel'
+      }
+    };
+    return directive;
+
+    function link(scope, element, attrs, ngModelCtrl) {
+      var display, cents;
+
+      ngModelCtrl.$render = function () {
+        display = $filter('number')(cents / 100, 2);
+
+        if (attrs.moneyMaskPrepend) {
+          display = attrs.moneyMaskPrepend + ' ' + display;
+        }
+
+        if (attrs.moneyMaskAppend) {
+          display = display + ' ' + attrs.moneyMaskAppend;
+        }
+
+        element.val(display);
+      }
+
+      scope.$watch('model', function onModelChange(newValue) {
+        newValue = parseFloat(newValue) || 0;
+
+        if(newValue !== cents) {
+          cents = Math.round(newValue * 100);
+        }
+
+        ngModelCtrl.$viewValue = newValue;
+        ngModelCtrl.$render();
+      });
+
+      element.on('keydown', function (e) {
+        var char = String.fromCharCode(e.keyCode);
+        e.preventDefault();
+        
+        if (char.search(/[0-9\-]/) === 0) {
+          cents = parseInt(cents + char);
+        }
+        else if (e.keyCode === 8) {
+          cents = parseInt(cents.toString().slice(0, -1)) || 0;
+        }
+        else {
+          return false;
+        }
+
+        ngModelCtrl.$setViewValue(cents / 100);
+        ngModelCtrl.$render();
+        scope.$apply();
+      })
+    }
+  }
+})();
